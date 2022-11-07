2ca41548520f474387b417be8343a4a4bba1ea55
Shreyas Vaishnav <83199543+ShreyasVaishnav@users.noreply.github.com>
2022-11-07 20:16:31 +0530

    Add files via upload

---
+++ my-first-commit.py
@@ -0,0 +1,11 @@
+# GitHub Commit_
+
+def addition_(num1, num2):
+    return num1 + num2 
+
+print("Hello!")
+num = input().split()
+num1 = int(num[0])
+num2 = int(num[1])
+
+print(f"Result : {addition_(num1, num2)}")
